fb615f56bd3b6669159000827d75b89e8d3829f6
here-and-now <jochmann.maximilian@gmail.com>
2021-11-04 19:03:44 +0100

    tables radiobutton group test

--- plot_hr.py
+++ plot_hr.py
@@ -4,39 +4,54 @@ from bokeh.models import HoverTool
 from bokeh.models.axes import LinearAxis
 from bokeh.layouts import layout
 from bokeh.io import show
-from bokeh.models import CustomJS, DateRangeSlider, ColumnDataSource
+from bokeh.models import CustomJS, DateRangeSlider, ColumnDataSource, CheckboxButtonGroup, RadioButtonGroup
 from bokeh.models.formatters import DatetimeTickFormatter
 from bokeh.plotting import figure, curdoc
 import pandas as pd
 from sqlalchemy import create_engine, inspect
 import datetime
+
+
 # connect sql db
 engine = create_engine('sqlite:///garmin_summary.db')
 cnx = engine.connect()
 
-
+# Read sql into dataframe
 df = pd.read_sql('intensity_hr', cnx,index_col='timestamp')
 # df.index= pd.to_datetime(df.index)
-# inspector = inspect(engine)
-# print(inspector.get_table_names())
+
+# Inspect table names
+inspector = inspect(engine)
+table_names = inspector.get_table_names()
+# column_names = [column.name for column in cnx.columns]
+
+column_names = inspector.get_columns('intensity_hr')
+
+print(table_names)
+print(column_names)
+
+radio_button_group = RadioButtonGroup(labels=table_names)
+
+
 
 source = ColumnDataSource(df)
 
 # bokeh plot stuff
 plot_w, plot_h = 1800, 500
 
-
 p = figure(title='Heart Rate',
             x_axis_type='datetime',
             plot_width=plot_w,
             plot_height=plot_h)
 
-p.line(x='timestamp', y='heart_rate', source=source)
+p.line(x='timestamp',
+       y='heart_rate',
+       source=source)
 
 today = datetime.date.today()
 date_range_slider = DateRangeSlider(value=(today - datetime.timedelta(days=7), today),
-                                    start=today - datetime.timedelta(days=30), end=today + datetime.timedelta(days=1))
-
+                                    start=today - datetime.timedelta(days=30),
+                                    end=today + datetime.timedelta(days=1))
 
 def update_plots(p):
     callback = CustomJS(args=dict(p=p), code="""
@@ -52,6 +67,7 @@ for plots in [p]:
 
 
 l = layout([
+            [radio_button_group],
             [date_range_slider],
             [p]
 ])
@@ -61,4 +77,3 @@ show(l)
 
 
 
-print(df)
