f14b963cd4599bb89d28ca917ac56c208aded1e8
here-and-now <jochmann.maximilian@gmail.com>
2021-11-07 23:28:29 +0100

    toggle button currently not working

--- plot_metrics.py
+++ plot_metrics.py
@@ -4,7 +4,7 @@ from bokeh.models import HoverTool
 from bokeh.models.axes import LinearAxis
 from bokeh.layouts import layout
 from bokeh.io import show
-from bokeh.models import CustomJS, DateRangeSlider, ColumnDataSource, CheckboxButtonGroup, RadioButtonGroup
+from bokeh.models import CustomJS, DateRangeSlider, ColumnDataSource, CheckboxButtonGroup, RadioButtonGroup, Toggle
 from bokeh.models.formatters import DatetimeTickFormatter
 from bokeh.plotting import figure, curdoc
 import pandas as pd
@@ -12,12 +12,9 @@ from sqlalchemy import create_engine, inspect
 import datetime
 import os
 from pathlib import Path
+from bokeh.models import Line
 
 from collections import defaultdict
-today = datetime.date.today()
-date_range_slider = DateRangeSlider(value=(today - datetime.timedelta(days=7), today),
-                                    start=today - datetime.timedelta(days=30),
-                                    end=today + datetime.timedelta(days=1))
 
 db_dict = {'garmin': 'garmin.db',
             'activities': 'garmin_activities.db',
@@ -25,24 +22,11 @@ db_dict = {'garmin': 'garmin.db',
             'garmin_summary': 'garmin_summary.db',
             'summary': 'summary.db'}
 
-def change_plot(attrname, old, new):
 
-    print('Selected x: ', x)
-    print('Selected y: ', y)
-    p.plot.xaxis.axis_label= x
-    p.plot.yaxis.axis_label= y
-    p.plot_height = 600
-    p.plot_width = 1800
-
-    for line in list(p.renderers):
-        p.renderers.remove(line)
-    # p.title(y)
-    p.line(x=x,
-           y=y,
-           source=source)
-
-    # update_plots(p)
-    
+today = datetime.date.today()
+date_range_slider = DateRangeSlider(value=(today - datetime.timedelta(days=7), today),
+                                    start=today - datetime.timedelta(days=30),
+                                    end=today + datetime.timedelta(days=1))
 def update_plots(p):
     callback = CustomJS(args=dict(p=p), code="""
         p.x_range.start = cb_obj.value[0]
@@ -51,7 +35,9 @@ def update_plots(p):
         """)
     return callback
 
-def load_dbs():
+
+# Callback function for DateRangeSlider
+def load_dbs(db_dict):
     recursive_dict = lambda: defaultdict(recursive_dict)
     dfs_dict = recursive_dict()
 
@@ -64,44 +50,64 @@ def load_dbs():
         inspector = inspect(engine)
         table_names = inspector.get_table_names()
         
-        if db == 'monitoring': 
-            idx_col='timestamp'
-        else:
-            idx_col = None
-
         for table in table_names:
             
-            dfs_dict[db][table] = pd.read_sql_table(table, cnx, index_col=idx_col)
-            dfs_dict[db][table].sort_index(inplace=True)
+            dfs_dict[db][table] = pd.read_sql_table(table, cnx)
             # print(dfs_dict[db][table])
     return dfs_dict
 
-def plot_anything(dfs_dict, db, table, x='timestamp', y=None, index_col=None):
-    dfs_dict[db][table]
+def plot_anything(dfs_dict, db, table, p, x='timestamp', y=None, index_col=None):
+    
+    if index_col == None:
+        index_col = x
+    
+    dfs_dict[db][table] = dfs_dict[db][table].set_index(index_col).sort_index()
     source = ColumnDataSource(dfs_dict[db][table])
     print(source.data) 
         
-    p = figure(x_axis_type='datetime')
-    p.line(x=x,y=y,source=source)
-    
+    p.line(x=x,y=y,source=source,name=y)
+
     return p
 
+def toggle_plots(p, name):
+    gr = p.select(name=name)
+    callback = CustomJS(args=dict(gr=gr), code="""
+        gr.visible = False
+        """)
+    print(gr)
+    print("SUP")
+    return callback
 
-dfs_dict = load_dbs()
-p = plot_anything(dfs_dict, 'monitoring', 'monitoring_hr', x='timestamp', y='heart_rate')
-# iniate dataframe and bokeh source
+# allback = CustomJS()
+
+p = figure(x_axis_type='datetime')
+
+dfs_dict = load_dbs(db_dict)
+p = plot_anything(dfs_dict, 'monitoring', 'monitoring_hr',p, x='timestamp', y='heart_rate')
+p = plot_anything(dfs_dict, 'garmin', 'stress',p, x='timestamp', y='stress')
+
 
 p.sizing_mode = 'scale_width'
 p.aspect_ratio = 3
+
+plots = [p]
+
+
+
+cb = toggle_plots(p, 'stress')
+toggle_stress = Toggle(label='Stress', button_type='success')
+toggle_stress.js_on_event('ButtonClick', cb)
+
 #date range slider callback
-for plot in [p]:
+for plot in plots:
     callback = update_plots(plot)
     date_range_slider.js_on_change('value_throttled', callback)
 
-# radio button group change listeners
+
 l = layout([
             [date_range_slider],
-            [p]
+            [p],
+            [toggle_stress]
 ])
 l.sizing_mode = 'scale_width'
 curdoc().add_root(l)
