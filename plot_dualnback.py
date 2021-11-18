3b663ce7a4c40ab9ceee7dae8d84fd832f839c71
here-and-now <jochmann.maximilian@gmail.com>
2021-11-18 11:21:33 +0100

    dualnback plot daily mean

--- plot_dualnback.py
+++ plot_dualnback.py
@@ -44,34 +44,53 @@ def plot_dnb(df):
     # df['norm'] = df['total_sleep'].apply(lambda x: x.hour * 60 + x.minute)
     # df['norm'] = (df['norm'] - df['norm'].min()) / (df['norm'].max() - df['norm'].min()) * 100
     
-    source = ColumnDataSource(df)
-    print(source.data)
-
+    nbacks = df['dnb'].unique()
+    uni_days = df.index.map(lambda x: x.date()).unique()
+    
     p = figure(x_axis_type='datetime')
-    p.scatter(x='time', y='percent', source=source)
+    for nback in nbacks:
+        df_sel = df.loc[df['dnb'] == nback]
+        group = df_sel.groupby([df_sel.index.date])
+
+        source = ColumnDataSource(df_sel)
+      
+        p.scatter(x=group.mean().index,
+                  y=group.mean()['score'],
+                  legend_label=nback) 
 
     return p
 
+# Pandas df stuff
 df = pd.read_csv(str(Path.home()) + '/.brainworkshop/data/stats.txt')
-df.columns = ['time', 'dnb', 'percent', 'mode', 'back', 'ticks_per_trial', 'num_trials_total','manual','session_number','pos1','audio','color','visvis','audiovis', 'arithmetic','image','visaudio','audio2','pos2','pos3','pos4','vis1','vis2','vis3','vis4','ticks_per_trial_times_tick_duration_times_num_trials_total','None']
-
+df.columns = ['time', 'dnb', 'percent', 'mode', 'back', 'ticks_per_trial', 'num_trials_total',
+              'manual','session_number','pos1','audio','color','visvis','audiovis', 'arithmetic',
+              'image','visaudio','audio2','pos2','pos3','pos4','vis1','vis2','vis3','vis4',
+              'ticks_per_trial_times_tick_duration_times_num_trials_total','None']
 df['time'] = pd.to_datetime(df['time'])
 df = df.set_index('time').sort_index()
-print(df) 
+df['score'] = df['back'] * 100 + df['percent']
+
+
+# plot stuff
 p = plot_dnb(df)
 
+p.sizing_mode ='scale_width'
+p.aspect_ratio = 3
+p.legend.location = 'top_right'
+p.legend.click_policy = 'hide'
 
-# for plot in [p]:
-    # callback = update_plots(plot)
-    # date_range_slider.js_on_change('value_throttled', callback)
+
+for plot in [p]:
+    callback = update_plots(plot)
+    date_range_slider.js_on_change('value_throttled', callback)
 
 # #Layout
 l = layout([
             [date_range_slider],
             [p],
 ])
-# # Scale layout
-# l.sizing_mode = 'scale_width'
+ # Scale layout
+l.sizing_mode = 'scale_width'
 
 
 curdoc().add_root(l)
